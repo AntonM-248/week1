1b6b9709ed9f680905836ed59358d458aeed1c47
AntonM-248 <53975792+AntonM-248@users.noreply.github.com>
2021-03-31 12:12:45 -0400

    initial commit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //allow the user to give upper & lower bound
+            //allow user to set condition for multiples
+            //print each num
+            //if number % 3 = 0, print fizz
+            //'          '5'          ' buzz
+            //fizzbuzz if both true
+
+            //my solution
+            Program prog = new Program();
+            //prog.fb(100);
+
+            //Business acceptable
+            prog.RunFizzBuzz(-100, 100, 4, 6, 10);
+        }
+
+        private void RunFizzBuzz(int start = 0, int end = 100, int num1 = 3, int num2 = 5, int inc = 1)
+        {
+            if (start > end)
+            {
+                System.Console.WriteLine("Your lower bound exceeds your upper bound, FizzBuzz will run for 0 loops");
+            }
+            for (var x = start; x <= end; x += inc)
+            {
+                PrintNumber(x, num1, num2);
+            }
+        }
+
+        private void PrintNumber(int a, int div1, int div2, string fizzer = "Fizz", string buzzer = "Buzz ")
+        {
+            string message = null;
+            if (a == 0)
+            {
+                WriteToConsole(a);
+                return;
+            }
+            if (a % div1 == 0)
+            {
+                message += fizzer;
+            }
+            if (a % div2 == 0)
+            {
+                message += buzzer;
+            }
+            WriteToConsole(a, message);
+        }
+
+        private void WriteToConsole(int num, string message = "")
+        {
+            // print to screen
+            System.Console.WriteLine($"{num} {message}");
+        }
+
+        private void fb(int num)
+        {//this solution wasn't compartmentalized enough
+         //failed to not fizzbuzz for 0
+            string f = "Fizz", b = "Buzz", result = null;
+            for (int i = 0; i <= num; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    result += f;
+                }
+                if (i % 5 == 0)
+                {
+                    result += b;
+                }
+                System.Console.WriteLine($"{i} {result}");
+                result = null;
+            }
+        }
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,34 @@
+﻿using sc = System.Console;
+
+
+
+namespace Revature.Week1.HelloWorld
+//namespace scope
+{
+    class Program
+    //class scope
+    {
+        private int Rpa(int num)
+        //method scope
+        {
+            var num1 = num;
+            return num1;
+        }
+
+        private void UiPath()
+        //another method scope
+        {
+            if (true)
+            //block scope
+            {
+
+            }
+        }
+
+        public static void Main()
+        {
+            var name = sc.ReadLine();
+            sc.WriteLine($"Hello {name}");
+        }
+    }
+}
